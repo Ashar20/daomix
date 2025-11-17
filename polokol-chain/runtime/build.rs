ff01fd2b762f768d58a7d1bfbf620fdc93c86cd6
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2025-11-18 03:10:02 +0530

    removed obselete files

--- polokol-chain/runtime/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    substrate_wasm_builder::WasmBuilder::new()
-        .with_current_project()
-        .import_memory()
-        .build();
-}
--- polokol-chain/runtime/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-
-pub mod constants;
-pub mod runtime;
-
-pub use runtime::*;
-
-#[cfg(feature = "std")]
-pub use runtime::{GenesisConfig, WASM_BINARY};
